8cef80c7e4517ea791988ac49dacdc3028b89dee
Chris Underwood <cunderwoodmn@gmail.com>
2016-12-12 21:36:38 -0800

    add start text to index, hides the board at startup, and shows the board when the start button is clicked.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 (function(){
   console.log("Inside the IIFE!");
+
+  //Hides the board to start the game
+  var hideBoard = document.getElementById('board');
+  hideBoard.style.display = 'none';
+
+  //When start button is clicked, the board is shown and the start screen is hidden.
+  var clickStartButton = document.getElementById('start-button');
+  clickStartButton.addEventListener("click", function (){
+    hideBoard.style.display = 'block';
+    var hideStart = document.getElementById('start');
+    hideStart.style.display = 'none';
+  });
+
  }());
