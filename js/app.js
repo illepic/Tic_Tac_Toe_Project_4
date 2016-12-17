edb74e2c265395822103396960806e678e7e8a4a
Chris Underwood <cunderwoodmn@gmail.com>
2016-12-16 18:30:26 -0800

    add js files to index. Moved code from app.js to game.js

--- js/app.js
+++ js/app.js
@@ -2,18 +2,8 @@
 
 (function(){
   //console.log("Inside the IIFE!");
-  "use strict";
+var game = new Game();
 
-  //Hides the board to start the game
-  var hideBoard = document.getElementById('board');
-  hideBoard.style.display = 'none';
-
-  //When start button is clicked, the board is shown and the start screen is hidden.
-  var clickStartButton = document.getElementById('start-button');
-  clickStartButton.addEventListener("click", function (){
-    hideBoard.style.display = 'block';
-    var hideStart = document.getElementById('start');
-    hideStart.style.display = 'none';
-  });
+  game.newGame();
 
  }());
